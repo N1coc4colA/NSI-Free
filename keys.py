5e0faeb5dd743249a12df08a663f95376ceb5e41
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2021-04-03 18:36:27 -1000

    Features added
    Now the window to choose the keys works well and is fully implemented.
    Basic backgrounds are now available
    runtime module's coponents have been improved:
    Runtime:
         - added routine support
         - added painting request system
         - better window handling
    Button:
         - Button now have clicked call back
    
    Added classes:
    Label, Button, Widget, SpriteObject (Update) and Scene

---
+++ keys.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+import runtime
+import pygame as pg
+
+global switcher
+
+switcher = {
+"p1_left": pg.K_LEFT,
+"p1_right": pg.K_RIGHT,
+"p1_up  ": pg.K_UP,
+"p1_down": pg.K_DOWN,
+"p1_at1 ": pg.K_KP_MINUS,
+"p1_at2 ": pg.K_KP_PLUS,
+"p1_at3 ": pg.K_KP_ENTER,
+"p2_left": pg.K_q,
+"p2_right": pg.K_d,
+"p2_up  ": pg.K_z,
+"p2_down": pg.K_s,
+"p2_at1 ": pg.K_TAB,
+"p2_at2 ": pg.K_a,
+"p2_at3 ": pg.K_e
+}
+
+class KeyButton(runtime.Button):
+    keyID = -1
+    _holds = False
+    _lastKey = None
+    def __init__(self):
+        runtime.Button.__init__(self)
+        self.setCallBack(self.onClicked)
+
+    def customPaint(self):
+        super().customPaint()
+
+    def setKey(self, key_id):
+        self.makePaintUpdate = True
+        self.text = pg.key.name(key_id)
+        self.keyID = key_id
+
+    def onClicked(self):
+        ""
+        if self.keyID != -1 and self._lastKey != None:
+            ""
+            global switcher
+            switcher[self.keyID] = self._lastKey
+            self.text = pg.key.name(self._lastKey)
+        self.makePaintUpdate = True
+        self._holds = False
+    
+    def update(self, event):
+        if super().update(event) == True:
+            if event != None and event.type == pg.KEYDOWN and self.clicked:
+                self._lastKey = event.key
+                return False
+        return True
+
+class KeysSettings(runtime.Widget):
+    _showing = False
+    _rtm = runtime.Runtime()
+    _win = None
+    closed = True
+
+    def __init__(self):
+        runtime.Widget.__init__(self)
+        self._rtm.addRoutine(self.postCheck)
+
+    def postCheck(self):
+        if self.closed == True and self._rtm.running == True:
+            self.closed = True
+            self._rtm.quit()
+
+    def popup(self):
+        if self._rtm.running == False:
+            self._win = pg.display.set_mode((600, 600))
+            self._rtm.setWindow(self._win)
+            self._rtm.addRoutine(self.postCheck)
+            self.closed = False
+            self.loadElements()
+            self._rtm.forceRePaint = True
+            self._rtm.execute()
+
+    def loadElements(self):
+        j1 = runtime.Label()
+        j2 = runtime.Label()
+
+        list1 = []
+        list2 = []
+        for i in range(0, 14):
+            list1.append(KeyButton())
+        for i in range(0, 7):
+            list2.append(runtime.Label())
+
+        j1.text = "Joueur 1"
+        j2.text = "Joueur 2"
+        j1.font = pg.font.SysFont(None, 64)
+        j2.font = pg.font.SysFont(None, 64)
+        j1.rect.x = 20
+        j2.rect.x = 320
+        
+        list2[0].text = "Aller à gauche";
+        list2[1].text = "Aller à droite";
+        list2[2].text = "Sauter";
+        list2[3].text = "Se baisser";
+        list2[4].text = "Attaque 1";
+        list2[5].text = "Attaque 2";
+        list2[6].text = "Attaque 3";
+        
+        global switcher
+        keys = list(switcher.values())
+        
+        self._rtm.appendObject(j1)
+        self._rtm.appendObject(j2)
+        for i in range (0, 14):
+            list1[i].rect.height = 40
+            list1[i].setKey(keys[i])
+            self._rtm.appendObject(list1[i])
+        for i in range(0, 7):
+            list2[i].rect.height = 20
+            self._rtm.appendObject(list2[i])
+
+        topBase = 60
+        padding = 10
+        for i in range(0, 7):
+            list2[i].font = pg.font.SysFont(None, 20)
+            list1[i].font = pg.font.SysFont(None, 40)
+            list1[i+7].font = pg.font.SysFont(None, 40)
+            list2[i].rect.x = 20
+            list2[i].rect.y = topBase + i*60 + i*padding
+            list1[i].rect.x = 20
+            list1[i+7].rect.x = 320
+            y = list2[i].rect.y + 20
+            list1[i].rect.y = y
+            list1[i+7].rect.y = y
+
+KeysSettings().popup()
--- runtime.py
+++ runtime.py
@@ -1,87 +1,286 @@
-import pygame
+#!/usr/bin/python3
+
+import pygame, sys
 pygame.init()
 
-class SpriteObject(pygame.sprite.Sprite):
+class Widget(pygame.sprite.Sprite):
     rect = pygame.Rect((0, 0), (200, 200))
     image = None
+    makePaintUpdate = False
 
     def __init__(self):
        # Call the parent class (Sprite) constructor
        pygame.sprite.Sprite.__init__(self)
 
-       #On défini la surface et on génère une image
        self.image = pygame.Surface([self.rect.width, self.rect.height])
        #On rempli l'image
        self.image.fill((0, 0, 0))
-       #On peut redéfinir la taille en fonction de l'image
        #self.rect = self.image.get_rect()
-
+    
     def setWindow(self, target):
         self.window = target
+    
+    def customPaint(self):
+        self.window.blit(self.image, (self.rect.x, self.rect.y))
+    
+    def update(self, event):
+        return True
+
+class Picture(Widget):
+    def __init__(self):
+        Widget.__init__(self)
+    
+    def customPaint(self):
+        size = self.image.get_size()
+        pygame.draw.rect(pygame.Surface(size, pg.SRCALPHA), (255, 255, 255), (0, 0, *size), border_radius=roundness)
+
+class ProgressBar(Widget):
+    maximum = 10
+    pos = 3
+    background = (0, 0, 0)
+    foreground = (255, 0, 0)
+    
+    def __init__(self):
+        Widget.__init__(self)
+    
+    def update(self, event):
+        return True
+    
+    def customPaint(self):
+        pygame.draw.rect(self.window, self.background, self.rect)
+        pygame.draw.rect(self.window, self.foreground, pygame.Rect((self.rect.x, self.rect.y), ((self.pos*self.rect.width/self.maximum), self.rect.height)))
+
+class Label(Widget):    
+    text = "Bonjour"
+    font = pygame.font.SysFont(None, 104)
+    color = (0, 0, 0)
+    
+    def __init__(self):
+        Widget.__init__(self)
+        self.rect = pygame.Rect((0, 0), (200, 50))
+    
+    def customPaint(self):
+        self.image = self.font.render(self.text, True, self.color)
+        self.window.blit(self.image, (self.rect.x, self.rect.y))
+    
+    def update(self, event):
+        return True
+
+class Button(Label):
+    clicked = False
+    background = (100, 100, 255)
+    background_clicked = (175, 175, 255)
+    _callBack = None
+    
+    def __init__(self):
+        Label.__init__(self)
+        self.font = pygame.font.SysFont(None, 54)
+
+    def setCallBack(self, func):
+        self._callBack = func
 
     def customPaint(self):
-        #On peut utiliser autres chose que ce qui est en dessous
-        self.window.blit(self.image, (self.rect.x, self.rect.y))
+        if self.clicked:
+           pygame.draw.rect(self.window, self.background_clicked, self.rect)
+        else:
+           pygame.draw.rect(self.window, self.background, self.rect)
+        self.image = self.font.render(self.text, True, self.color)
+        text_width, text_height = self.font.size(self.text)
+        self.window.blit(self.image, (self.rect.x + ((self.rect.width-text_width)/2), self.rect.y + ((self.rect.height-text_height)/2)))
+    
+    def update(self, event):
+        if event != None and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if self.rect.collidepoint(pygame.mouse.get_pos()):
+                self.clicked = True
+                self.makePaintUpdate = True
+            else:
+                if self.clicked:
+                    self.clicked = False
+                    self.makePaintUpdate = True
+                    return True
+            return False
+        elif event != None and event.type == pygame.MOUSEBUTTONUP:
+            if self.clicked:
+                self.clicked = False
+                self.makePaintUpdate = True
+                if self._callBack != None:
+                    self._callBack()
+            return False
+        return True
+
+class SpriteObject(Widget):
+    leftKey = pygame.K_LEFT
+    rightKey = pygame.K_RIGHT
+    upKey = pygame.K_UP
+    downKey = pygame.K_DOWN
+    sourceTicks = pygame.time.get_ticks()
+    updateTimer = True
+    
+    def __init__(self):
+        Widget.__init__(self)
+    
+    def handleLeft(self):
+        if ((pygame.time.get_ticks() - self.sourceTicks)/1000)>0.05:
+            self.rect.x = self.rect.x -2
+            self.updateTimer = True
+        
+    def handleRight(self):
+        if ((pygame.time.get_ticks() - self.sourceTicks)/1000)>0.05:
+            self.rect.x = self.rect.x +2
+            self.updateTimer = True
+    
+    def handleUp(self):
+        ""
+    
+    def handleDown(self):
+        ""
+    
+    def update(self, event):
+        if event == None or event.type == pygame.KEYDOWN:
+            pressed_keys = pygame.key.get_pressed()
+            if pressed_keys[self.leftKey]:
+                self.handleLeft()
+            if pressed_keys[self.rightKey]:
+                self.handleRight()
+            if pressed_keys[self.upKey]:
+                self.handleUp()
+            if pressed_keys[self.downKey]:
+                self.handleDown()
+            if self.updateTimer:
+                self.sourceTicks = pygame.time.get_ticks()
+                self.updateTimer = False
+            self.makePaintUpdate = True
+            print("paint")
+            return (event != None)
+        return False
+
+class Scene(Widget):
+    scaled = None
+
+    def __init__(self):
+        Widget.__init__(self)
+
+    def setImage(self, path):
+        self.image = pygame.image.load(path)
+        if self.window != None:
+            self.scaled = pygame.transform.scale(self.image, (self.window.get_width(), self.window.get_height()))
+
+    def customPaint(self):
+        if self.scaled != None:
+            self.window.blit(self.scaled, self.rect)
 
     def update(self, event):
-        #On fait des choses en fontcon du type d'événements
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint(event.pos):
-                print("collided")
-        if (event.type == pygame.KEYDOWN):
-            self.rect.x = self.rect.x +2
-        self.customPaint()
-        #Pour savoir si les autres éléments qui sont en dessous doivent aussi recevoir l'événement
-        return False
+        if self.window != None:
+            if self.window.get_height != self.rect.height or self.window.get_width != self.rect.width:
+                self.rect.width = self.window.get_width()
+                self.rect.height = self.window.get_height()
+                if self.image != None:
+                    self.scaled = pygame.transform.scale(self.image, (self.window.get_width(), self.window.get_height()))
+                return True
+        return True
+            
 
 class Runtime:
     objectList = []
     target_win = None
-    keepRunning = True
-
+    running = False
+    forceRePaint = False
+    _firstLoad = True
+    _routines = []
+    
     def appendObject(self, obj):
         obj.setWindow(self.target_win)
         self.objectList.append(obj)
-
+    
     def setWindow(self, win):
         self.target_win = win
         for obj in self.objectList:
             obj.setWindow(win)
             obj.update()
-
+    
     def paintWindow(self):
-        #Mettre à jour la fenêtre
-        pygame.draw.rect(window, (200, 200, 200),(0, 0, 300, 600))
-        pygame.draw.rect(window, (255, 255, 255),(300, 0, 300, 600))
-
+        pygame.draw.rect(self.target_win, (200, 200, 200),(0, 0, 300, 600))
+        pygame.draw.rect(self.target_win, (255, 255, 255),(300, 0, 300, 600))
+    
+    def addRoutine(self, func):
+        self._routines.append(func)
+    
+    def postRoutine(self):
+        ""
+    
+    def preRoutine(self):
+        ""
+    
     def quit(self):
-        self.keepRunning = False;
-
-    def exec(self):
+        self.running = False;
+    
+    def execute(self):
         # Main Loop
-        while self.keepRunning:
-            #Events handler. On filtre les événements, puis on les envoi vers les objets concernés
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit(1)
-                else:
-                    self.paintWindow()
-                    #Envoyer les événements aux objets
-                    run = True
-                    i = 0
-                    while run:
-                        if self.objectList[i] != None:
-                            run = self.objectList[i].update(event)
-                        else:
-                            run = False
-                        i+=1
+        self.running = True
+        while self.running:
+            self.preRoutine()
+            
+            for routine in self._routines:
+                routine()
+            
+            eventList = pygame.event.get()
+            shouldRePaint = False
+            if not eventList or self._firstLoad:
+                run = True
+                i = 0
+                while run and i<len(self.objectList):
+                    if self.objectList[i] != None:
+                        run = self.objectList[i].update(None)
+                        if self.objectList[i].makePaintUpdate == True:
+                            shouldUpdate = True
+                    else:
+                        run = False
+                    i+=1
+            else:
+                #Share the events
+                for event in eventList:
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit(1)
+                    else:
+                        run = True
+                        i = 0
+                        #If an object accepts it, it means others must not get it: stop the loop
+                        while run and i<len(self.objectList):
+                            if self.objectList[i] != None:
+                                run = self.objectList[i].update(event)
+                                if self.objectList[i].makePaintUpdate == True:
+                                    shouldUpdate = True
+                            else:
+                                run = False
+                            i+=1
+            #Make new paintings if needed
+            if (shouldRePaint == True) or (self.forceRePaint == True) or (self._firstLoad == True) :
+                if self._firstLoad:
+                    self._firstLoad = False
+                self.paintWindow()
+                continuePainting = True
+                j = 0
+                while continuePainting and j<len(self.objectList):
+                    if self.objectList[j] != None:
+                        self.objectList[j].customPaint()
+                        self.objectList[j].makePaintUpdate = False
+                    else:
+                        continuePainting = False
+                    j+=1
+        
             pygame.display.update()
+            self.postRoutine()
 
+"""
+        TEMPLATE:
 pygame.display.set_caption('Crash!')
 window = pygame.display.set_mode((600, 600))
-
+scene = Scene()
 rtm = Runtime()
 rtm.setWindow(window)
-rtm.appendObject(SpriteObject())
-rtm.exec()
+#rtm.appendObject(SpriteObject())
+rtm.appendObject(scene)
+scene.setImage("./backgrounds//bamboo.png")
+rtm.execute()
+"""
