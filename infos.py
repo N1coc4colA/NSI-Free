9c845b052241a92a3de486c049bca4052f9680d0
teavanui <71474783+teavanui@users.noreply.github.com>
2021-05-31 08:23:25 -1000

    Update infos.py

--- infos.py
+++ infos.py
@@ -66,22 +66,14 @@ class InfosWindow(runtime.Widget):
 		l4 = runtime.Label()
 		l5 = runtime.Label()
 		l6 = runtime.Label()
-		l7 = runtime.Label()
-		l8 = runtime.Label()
-		l9 = runtime.Label()
-		l10 = runtime.Label()
 
 		l2.text = "Ici, deux joueurs s'affrontent en 1v1."
 		l3.text	= "Chacun a un personnage avec différentes attaques. Cependant, il n'y en a seulement 4,"
 		l4.text	= "pour les appeler, vérifiez vos touches.La barre de vie, les sauts, kicks et autres"
 		l5.text	= "coups en tout genre seront vos seules options pour battre les doigts qui sont posés"
 		l6.text	= "sur le même clavier."
-		l7.text	= "Mais pourquoi ''Pringle's Fight''?"
-		l8.text	= "De temps en temps, un Pringle se ballade sur votre écran, donnant accès à un coup spé-"
-		l9.text	= "cial pour domminer votre concurant, seulement, vous avez 5 secondes pour l'activer et"
-		l10.text	= "la touche à utiliser est choisie au hasard, alors la map vous montrera ses secrets!"
-
-		listed = [l2, l3, l4, l5, l6, l7, l8, l9, l10]
+		
+		listed = [l2, l3, l4, l5, l6]
 		i = 0
 		top = 68
 		left = 15
